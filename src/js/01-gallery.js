0a214d2127ab52f2c024ec83055ec234e8f5d9c6
valentyn1407 <valikberkovskyi54.6@gmail.com>
2023-09-02 21:56:42 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,7 +7,7 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
-let lightbox = null;
+
 
 const renderImage = (arr, container) => {
     const marcup = arr.map((item) => `<a class="gallery-link" href="${item.original}">
@@ -15,32 +15,13 @@ const renderImage = (arr, container) => {
     </a>`).join('');
 
     container.innerHTML = marcup;
-}
+ }
 
 renderImage(galleryItems, galleryList);
 
-
-galleryList.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-
-    if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a', {
-            captions: true,
-            captionsData: 'alt',
-            captionPosition: 'bottom',
-            captionDelay: 250
+ const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250
         });
-    }
-
-    lightbox.open();
-});
-
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape' && lightbox) {
-        lightbox.close();
-    }
-});
